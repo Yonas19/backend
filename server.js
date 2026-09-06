f74c17bcb733f821afaca2b5b9b324ef9b956660
Yonas19 <yonjr0936@gmail.com>
2026-09-07 03:06:32 +0400

    Stage 1: root and health endpoints

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3009;
 
-app.get('/',(req, res) => {
-    res.send("Hello World!");
-})
+app.get("/", (req, res) => {
+  res.json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"],
+  });
+});
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
-
+  console.log(`server is running on port ${port}`);
+});
