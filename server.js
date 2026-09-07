665fc93da178102eabc284a9ade8e8625f5af4a5
Yonas19 <yonjr0936@gmail.com>
2026-09-07 04:23:13 +0400

    Stage 3: create with validation

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ const tasks = [
   },
 ];
 
+const { error } = require("console");
 const express = require("express");
 const app = express();
 const port = 3009;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
@@ -46,6 +48,27 @@ app.get("/tasks/:id", (req, res) => {
   }
   res.json(task);
 });
+
+app.post("/tasks", (req, res) => {
+    const { title } = req.body;
+    
+  if (!title || title.trim() === "") {
+    return res.status(400).json({
+      error: "Title is required",
+    });
+  }
+
+    const nextId = tasks.length + 1;
+  const newTask = {
+    id: nextId,
+    title: title.trim(),
+    done: false,
+  };
+
+  tasks.push(newTask);
+  return res.status(201).json(newTask);
+});
+
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
