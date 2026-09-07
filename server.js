970ffa2215661bad61ca3ba2e74916e3efc6817c
Yonas19 <yonjr0936@gmail.com>
2026-09-07 04:37:32 +0400

    Stage 4: full CRUD

--- server.js
+++ server.js
@@ -69,6 +69,79 @@ app.post("/tasks", (req, res) => {
   return res.status(201).json(newTask);
 });
 
+
+
+
+
+app.put("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const task = tasks.find((task) => task.id === id);
+
+  if (!task) {
+    return res.status(404).json({
+      error: `Task ${id} not found`,
+    });
+  }
+
+  const { title, done } = req.body;
+
+
+  if (title === undefined && done === undefined) {
+    return res.status(400).json({
+      error: "Provide title or done",
+    });
+  }
+
+ 
+  if (
+    title !== undefined &&
+    (typeof title !== "string" || title.trim() === "")
+  ) {
+    return res.status(400).json({
+      error: "Title must be a non-empty string",
+    });
+  }
+
+ 
+  if (done !== undefined && typeof done !== "boolean") {
+    return res.status(400).json({
+      error: "Done must be true or false",
+    });
+  }
+
+  if (title !== undefined) {
+    task.title = title.trim();
+  }
+
+  if (done !== undefined) {
+    task.done = done;
+  }
+
+  return res.status(200).json(task);
+});
+
+
+
+app.delete("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+
+  if (taskIndex === -1) {
+    return res.status(404).json({
+      error: `Task ${id} not found`,
+    });
+  }
+
+  tasks.splice(taskIndex, 1);
+
+  return res.status(204).send();
+});
+
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
+
+
+
